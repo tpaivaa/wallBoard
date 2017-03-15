dcbdf8e407c10b5c0f1e190cdb8625b558f6104f
Tomi Hakkarainen <tomi.hakkarainen@vintor.fi>
2017-03-15 09:20:38 +0200

    logia wallBoardData

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(function (req, res, next) {
 
 app.get('/wallBoardData',(req, res, next) => {
 	var jsonData = reload('./public/jsonData')
-	//log(jsonData)
+	log(jsonData)
 	res.json(jsonData)
 })
 
