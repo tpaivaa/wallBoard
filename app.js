8be2170c62e1b7b818de1c1cc9d639a9f63e76a6
Tomi Hakkarainen <tomi.hakkarainen@vintor.fi>
2017-04-10 14:00:14 +0300

    suomi otsikot

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var log = console.log
 var bodyParser = require('body-parser');
 
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 // Pretty print JSON responses
