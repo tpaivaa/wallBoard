ce2e72dd814ff0a1ea47958b66d6d82ef3815f8e
tpaivaa <tpaivaa@gmail.com>
2017-09-05 18:53:55 +0300

    armin muutos pyyntö

--- public/js/index.js
+++ public/js/index.js
@@ -4,7 +4,7 @@ wallboard.controller('wallCtrl', ['$scope','$http','$timeout','$location', funct
 	$scope.headers = {};
 	$scope.details = {};
 	$scope.site = location.search.split('site=')[1];
-	$scope.waitColorAsiakaspalvelu = {"background-color":"red"};
+	
 
 	var styles1 = ["{'background-color':'white'}","{'background-color':'red'}"];
 	var styles = ['label-pill label-white', 'label-pill  label-success',' label-pill label-info',' label-pill label-warning','label-pill label-danger', ''];
@@ -63,6 +63,7 @@ wallboard.controller('wallCtrl', ['$scope','$http','$timeout','$location', funct
 		$http.get(getDataURL)
 		.then(function(response){ 
 			$scope.details = response.data;
+			if (response.data["Asiakaspalvelu"].waiting > 4) {$scope.waitColorAsiakaspalvelu = {"background-color":"red"};}
 		});
 	}
 
